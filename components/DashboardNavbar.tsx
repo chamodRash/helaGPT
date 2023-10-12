603c2e0c8f2fa9502be40e0d763847cd7e1a3e82
chamodRash <cnilwakka@gmail.com>
2023-10-13 04:35:42 +0530

    modifications on mobile view dashboard navbar

--- components/DashboardNavbar.tsx
+++ components/DashboardNavbar.tsx
@@ -44,8 +44,8 @@ export default function DashboardNavbar() {
   const t = useTranslations("dashboardNavbar");
 
   return (
-    <div className="w-11/12 h-[13%] md:h-1/6 mx-auto flex items-center justify-between md:justify-end">
-      <div className="md:hidden ml-5 w-52 flex items-center gap-4">
+    <div className="w-11/12 h-[8%] md:h-1/6 mx-auto flex items-center justify-between md:justify-end">
+      <div className="md:hidden ml-1 w-52 flex items-center gap-4">
         <Sheet>
           <SheetTrigger>
             <Button
@@ -55,19 +55,19 @@ export default function DashboardNavbar() {
               <RxHamburgerMenu className="text-lg" />
             </Button>
           </SheetTrigger>
-          <SheetContent side={"left"} className="bg-zinc-950">
+          <SheetContent side={"left"} className="bg-zinc-50 dark:bg-zinc-950">
             <DashboardSidebar />
           </SheetContent>
         </Sheet>
         <h1 className="text-lg font-extrabold tracking-wide">HelaGPT</h1>
       </div>
 
-      <div className="w-[200px] md:w-[400px] flex items-center justify-end mr-5 gap-5">
+      <div className="w-[200px] md:w-[400px] flex items-center justify-end mr-1 md:mr-5 gap-5">
         <Button
           size={"icon"}
-          variant={"ghost"}
-          className="md:hidden ml-5 bg-zinc-50 dark:bg-zinc-700 rounded-full">
-          <AiOutlinePlus className="text-white text-lg" />
+          variant={"outline"}
+          className="md:hidden ml-5  border border-zinc-400 dark:bg-zinc-700 rounded-full">
+          <AiOutlinePlus className="text-zinc-900 dark:text-white text-lg" />
         </Button>
 
         <DropdownMenu>
@@ -113,7 +113,7 @@ export default function DashboardNavbar() {
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-64 mt-3 mr-5 text-center border-2 dark:bg-zinc-950 dark:border-black">
-            <div className="cursor-pointer pb-3 pl-2 w-full flex items-center justify-between text-zinc-600 hover:text-zinc-900 dark:text-white">
+            <div className="md:hidden cursor-pointer pb-3 pl-2 w-full flex items-center justify-between text-zinc-600 hover:text-zinc-900 dark:text-white">
               <DropdownMenu>
                 <DropdownMenuTrigger
                   asChild
@@ -152,7 +152,7 @@ export default function DashboardNavbar() {
 
               <ThemeToggler device="mobile" />
             </div>
-            <div className="cursor-pointer py-3 pl-2 border-t border-zinc-200 dark:border-zinc-800 flex items-center gap-3 text-zinc-600 hover:text-zinc-900 dark:text-white">
+            <div className="cursor-pointer py-3 md:pt-0 pl-2 border-t md:border-t-0 border-zinc-200 dark:border-zinc-800 flex items-center gap-3 text-zinc-600 hover:text-zinc-900 dark:text-white">
               <FaUserGear className="text-lg" />
               <p>{t("userActions.myAccount")}</p>
             </div>
--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -1,7 +1,14 @@
 "use-client";
 
+import Image from "next/image";
+import LogoWithText from "@/public/logoWithText.png";
+
 const DashboardSidebar = () => {
-  return <>SideBar</>;
+  return (
+    <>
+      <Image src={LogoWithText} width={150} height={150} alt="Logo" />
+    </>
+  );
 };
 
 export default DashboardSidebar;
